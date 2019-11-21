068fc194e38b04b1eee618407f740dd121fc9adf
qqq12597 <hieuqqq12589@gmail.com>
2019-11-22 01:49:00 +0700

    -update category.html, add category_laptop.html, category_smartphone.html, category.js

---
+++ Online Auction/js/category.js
@@ -0,0 +1,30 @@
+
+//time down
+var i = 0
+var length = 12
+var days,hours,minutes,seconds 
+var distance
+var countDownDate=new  Array(length)
+var allTimeRemaining = $(document).find('.product_time_remaining')
+
+for (i = 0; i < allTimeRemaining.length; i++) {
+    countDownDate[i] = new Date(allTimeRemaining[i].textContent).getTime();
+}
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    for (i = 0; i < length; i++) {
+         distance = countDownDate[i] - now;
+         days = Math.floor(distance / (1000 * 60 * 60 * 24));
+         hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+         minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+         seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        if (days==0) {
+        		 allTimeRemaining[i].innerHTML =  hours + "h " +  minutes + "m " + seconds + "s ";
+        }else
+        allTimeRemaining[i].innerHTML = days + "d " + hours + "h " +  minutes + "m " + seconds + "s ";
+        if (distance < 0) {
+            allTimeRemaining[i].innerHTML = "ENDING";
+        }
+    }
+
+}, 1000);
