58264c38aec86ab8e955edb54284b0c3ba6514d4
qqq12597 <hieuqqq12589@gmail.com>
2019-11-24 00:28:39 +0700

    -update categories, admin

--- Online Auction/js/category.js
+++ Online Auction/js/category.js
@@ -1,5 +1,5 @@
 
-//time down
+//count down
 var i = 0
 var length = 12
 var days,hours,minutes,seconds 
@@ -28,3 +28,45 @@ var x = setInterval(function() {
     }
 
 }, 1000);
+
+
+
+
+
+var tableData = [{
+        'name': 'Russell',
+        'price': '$150',
+        'time': 'November 20, 2019 20:15:10',
+    }]
+
+
+function buildTable() {
+    var table =$('#data')
+
+ // var row=`<div class="product-item  smartphone">
+ //                                        <div class="product product_filter">
+ //                                            <div class="product_image">
+ //                                                <img src="images/product_1.jpg" alt="">
+ //                                            </div>
+ //                                            <div class="favorite favorite_left"></div>
+ //                                            <div class="product_info">
+ //                                                <h6 class="product_name"><a href="single.html">${tableData[0].name}
+                                                
+ //                                                </a></h6>
+ //                                                <div class="product_price">${table[0].price}</div>
+ //                                                <div class="product_time_remaining">${table[0].time}</div>
+ //                                            </div>
+ //                                        </div>
+ //                                        <div class="red_button auction_button"><a href="#">
+ //                                                <img src="images/auction.png" alt="">
+ //                                            </a></div>
+ //                                    </div>
+ //                                    `
+
+var row=` <div>123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123
+                                    
+                                </div>`
+
+        table.append(row)
+
+}
