3cfeaa0f507fbd60b8f0beeb7b24a79a7a822227
hienhhcc <55795392+hienhhcc@users.noreply.github.com>
2019-12-18 11:42:28 +0700

    Update nếu username tạo đã tồn tại

--- Online Auction/models/user.model.js
+++ Online Auction/models/user.model.js
@@ -3,6 +3,7 @@ const db = require('../utils/db');
 module.exports = {
   all: () => { db.load('select * from user'); },
   single: id => db.load(`select * from user where userID = ${id}`),
+  single1: name => db.load1(`user`, { username: name }),
   add: entity => db.add('user', entity),
   del: id => db.del('user', { userID: id }),
 };
--- Online Auction/routes/account.route.js
+++ Online Auction/routes/account.route.js
@@ -10,29 +10,40 @@ router.get('/register', async (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-    const N = 10;
-    const hash = bcrypt.hashSync(req.body.raw_password, N);
-    const dob = moment(req.body.user_dob, 'DD/MM/YYYY').format('YYYY-MM-DD');
+    //Kiểm tra trong db đã có user có username trùng không
+    const checkdb = await userModel.single1(req.body.user_name);
+    console.log(req.body.user_name);
+    if (checkdb[0] == undefined){
+        const N = 10;
+        const hash = bcrypt.hashSync(req.body.raw_password, N);
+        const dob = moment(req.body.user_dob, 'DD/MM/YYYY').format('YYYY-MM-DD');
 
-    const entity = req.body;
-    entity.username = req.body.user_name;
-    entity.password = hash;
-    entity.firstname = req.body.first_name;
-    entity.lastname = req.body.last_name;
-    entity.email=req.body.user_email;
-    entity.dob = dob;
-    entity.type = 0;
+        const entity = req.body;
+        entity.username = req.body.user_name;
+        entity.password = hash;
+        entity.firstname = req.body.first_name;
+        entity.lastname = req.body.last_name;
+        entity.email = req.body.user_email;
+        entity.dob = dob;
+        entity.type = 0;
 
-    delete entity.user_name;
-    delete entity.raw_password;
-    delete entity.first_name;
-    delete entity.last_name;
-    delete entity.user_email;
-    delete entity.user_dob;
-    delete entity.user_address;
+        delete entity.user_name;
+        delete entity.raw_password;
+        delete entity.first_name;
+        delete entity.last_name;
+        delete entity.user_email;
+        delete entity.user_dob;
+        delete entity.user_address;
 
-    const result = await userModel.add(entity);
-    res.render('vwAccount/register');
+        const result = await userModel.add(entity);
+        res.render('vwAccount/register',{
+            hasUserNameInDatabase: false,
+        });
+    }else{
+        res.render('vwAccount/register', {
+            hasUserNameInDatabase: true,
+        });
+    }
 });
 
 module.exports = router;
