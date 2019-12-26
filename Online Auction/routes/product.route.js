a01748b045a574256f177429a2ea2ffe33e9f4fa
qqq12597 <hieuqqq12589@gmail.com>
2019-12-27 01:48:23 +0700

    update bid,review

--- Online Auction/routes/product.route.js
+++ Online Auction/routes/product.route.js
@@ -2,46 +2,53 @@ const express = require('express');
 const productModel = require('../models/product.model');
 const router = express.Router();
 
-
 router.get('/id=:id', async (req, res) => {
-  const proid=req.params.id;
-  const [prd,sli,cwi,g4,review,relatedPrd] = await Promise.all([
-     productModel.single(proid),
-     productModel.getSellerInfo(proid),
-     productModel.getCurrentWinner(proid),
-     productModel.get3TimesLatestPrice(proid),
-     productModel.getReview(proid),
-     productModel.get5RelatedProduct(proid)
+  const proid = req.params.id;
+  const [prd, sli, cwi, g4, review, relatedPrd] = await Promise.all([
+    productModel.single(proid),
+    productModel.getSellerInfo(proid),
+    productModel.getCurrentWinner(proid),
+    productModel.get3TimesLatestPrice(proid),
+    productModel.getReview(proid,3),
+    productModel.get5RelatedProduct(proid)
   ]);
+  var reviewLength=0;
+  if (review[0].length!=0)  reviewLength=review[0][0].CountRevByID;
+  console.log(review);
+  console.log( prd[0].length === 0);
   res.render('vwSingleProduct/single', {
-    
+    Productid: proid,
     product: prd[0],
     PrdEmpty: prd[0].length === 0,
-    sellerInfo:sli[0],
-    curWinnerInfo:cwi[0],
-    InfoLastestAuction:g4[0],
-    review:review[0],
-    reviewLength:review[0].length,
-    relatedPrd:relatedPrd[0]
+    sellerInfo: sli[0],
+    curWinnerInfo: cwi[0],
+    InfoLastestAuction: g4[0],
+    review: review[0],
+    reviewLength:reviewLength,
+    relatedPrd: relatedPrd[0]
   });
-});
 
-router.post('/id=:id',async (req, res) => {
-  var date=new Date();
-  const entity=req.body;
-  entity.revID=null;
-  entity.proID=3;
-  entity.userID=3;
-  entity.ratting=4,
-  entity.time_post=date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+});
+router.post('/id=:id', async (req, res) => {
+  if (req.body.key==='bid')
+  {
+    delete req.body.key;
+    await productModel.fAuction(req.body.ProductId,req.body.UserID,req.body.Price);
+    return res.redirect('back');
+  }
+else 
+{
+  const date = new Date();
+  const entity = req.body;
+  delete entity.key;
+  entity.ReviewID = null;
+  entity.TimePost = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
     date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
   console.log(entity);
-  alert("This is alert box!");
-   const result=await productModel.addReview(entity);
-   console.log(request.body.comment);
-   res.render('vwSingleProduct/single');
+  const result = await productModel.addReview(entity);
+  return res.end();
+}
 })
 
 module.exports = router;
-
 
