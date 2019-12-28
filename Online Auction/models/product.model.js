4e4bcc9a4e25db7a733bd4b0272167aed3b0d3d5
viethoang <viethoang.hcmus@gmail.com>
2019-12-28 19:10:03 +0700

    finshed panigation product by category

--- Online Auction/models/product.model.js
+++ Online Auction/models/product.model.js
@@ -1,4 +1,5 @@
 const db = require('../utils/db');
+const config = require('../config/default.json');
 
 module.exports = {
   all: () => db.load('select * from Product'),
@@ -6,7 +7,20 @@ module.exports = {
       select * from Product p join Categories c
       on p.CatID = c.CatID
       where p.CatID = ${catId} or c.ParentID = ${catId}`),
-
+  countByCat: async catId => {
+    const rows = await db.load(`
+    select count(p.ProductID) as total 
+    from Product p join Categories c
+    on p.CatID = c.CatID
+    where p.CatID = ${catId} and p.CatID = c.CatID  and c.ParentID != 0
+    or  (p.CatID = c.CatID and c.ParentID = ${catId})`)
+    return rows[0].total;
+  },
+  pageByCat: (catId, offset) => db.load(`
+  select * from Product p join Categories c
+      on p.CatID = c.CatID
+      where p.CatID = ${catId} or c.ParentID = ${catId}
+      limit ${config.paginate.limit} offset ${offset}`),
   single: id => db.load(`CALL getSingleProduct(${id})`),
   getSellerInfo: id => db.load(`CALL getSellerInfo(${id});`),
   getCurrentWinner: id=> db.load(`CALL getCurrentWinner(${id}) `),
--- Online Auction/utils/db.js
+++ Online Auction/utils/db.js
@@ -1,14 +1,8 @@
 const mysql = require('mysql');
 const util = require('util');
+const config = require('../config/default.json');
 
-const pool = mysql.createPool({
-  connectionLimit: 50,
-  host: 'sql12.freemysqlhosting.net',
-  port: 3306,
-  user: 'sql12316883',
-  password: '2H7naDWhDM',
-  database: 'sql12316883'
-});
+const pool = mysql.createPool(config.mysql);
 
 const mysql_query = util.promisify(pool.query).bind(pool);
 
