f3c13ba6df7dbfae806f1aacd086907ad5ee871b
viethoang <viethoang.hcmus@gmail.com>
2019-12-30 19:13:52 +0700

    fix category: set card product full infomation

--- Online Auction/models/product.model.js
+++ Online Auction/models/product.model.js
@@ -21,10 +21,19 @@ module.exports = {
     return rows[0].total;
   },
   pageByCat: (catId, offset) => db.load(`
-  select * from Product p join Categories c
-      on p.CatID = c.CatID
-      where p.CatID = ${catId} or c.ParentID = ${catId}
-      limit ${config.paginate.limit} offset ${offset}`),
+  SELECT p4.ProductID, p4.ProductName, p4.PriceStart,p4.PricePurchase,
+  p4.TimeExp, p4.NumBid,p4.CurrentWinner, concat(u.FirstName," ", u.LastName) as WinnerName
+  FROM (SELECT p3.ProductID, p3.ProductName, p3.PriceStart,p3.PricePurchase,
+        p3.TimeExp, p2.NumBid,p3.CurrentWinner
+        FROM (SELECT p1.ProductID, p1.CurrentWinner, COUNT(b.BidID) as NumBid
+        FROM Bid b RIGHT JOIN Product p1 ON b.ProID = p1.ProductID
+        GROUP BY p1.ProductID,p1.CurrentWinner) p2, Product p3, Categories c
+        WHERE p2.ProductID = p3.ProductID and p3.CatID = c.CatID and (p3.CatID = ${catId} or c.ParentID = ${catId})
+        limit ${config.paginate.limit} offset ${offset}) p4 
+  left join Users u
+  on u.UserID = p4.CurrentWinner
+  `
+  ),
   single: id => db.load(`CALL getSingleProduct(${id})`),
   getSellerInfo: id => db.load(`CALL getSellerInfo(${id});`),
   getCurrentWinner: id => db.load(`CALL getCurrentWinner(${id}) `),
